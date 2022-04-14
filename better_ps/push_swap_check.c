7af924801a50414185e7f0d6bc5b761935efc4d1
KinetikMalizia <94684576+KinetikMalizia@users.noreply.github.com>
2022-04-14 15:38:14 +0200

    need to fix rra and implement *PREVIOUS*

---
+++ better_ps/push_swap_check.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_check.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcarlen <jcarlen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/06 11:16:00 by jeancarlen        #+#    #+#             */
+/*   Updated: 2022/04/13 16:30:17 by jcarlen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+
+int	check_if_sorted(int ac, char **av)
+{
+	int	i;
+	int	len;
+
+	i = 1;
+	len = (ac - 2);
+	while (i < len)
+	{
+		if (av[i] > av[i + 1])
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	check_if_dup(int ac, char **av)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < (ac - 2))
+	{
+		j = (i + 1);
+		while (j < (ac - 1))
+		{
+			if (av[i] == av[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
