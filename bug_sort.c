c186c62230e143145e8f5e169274f2eb42740c8c
KineticKangaroo <94684576+KinetikMalizia@users.noreply.github.com>
2022-04-27 15:23:19 +0200

    everything is pain

--- bug_sort.c
+++ bug_sort.c
@@ -12,18 +12,30 @@
 
 #include "push_swap.h"
 
+typedef struct s_place
+{
+	t_list	*top_a;
+	t_list	*top_b;
+	t_list	*bot_a;
+	t_list	*bot_b;
+	t_list	*min_b;
+	t_list	*max_a;
+	t_list	*start;
+}					t_place;
+
+
 void big_sort(t_list **stack_a, t_list **stack_b)
 {
 	int		bit;
 	int		max;
 	t_list	*ptr;
 	t_list	*start;
-	
+
 	bit = 0;
 	start = NULL;
-	while(bit <= 8)
+	while (bit <= 8)
 	{
-		while(*stack_a != start && *stack_a)
+		while (*stack_a != start && *stack_a)
 		{
 			ptr = *stack_a;
 			if (ptr->content >> bit & 1)
@@ -34,7 +46,7 @@ void big_sort(t_list **stack_a, t_list **stack_b)
 		}
 		//print_lst(stack_b);
 		start = NULL;
-		while(*stack_b)
+		while (*stack_b)
 		{
 			pa(stack_a, stack_b);
 		}
@@ -44,3 +56,28 @@ void big_sort(t_list **stack_a, t_list **stack_b)
 /* gerer les 7premier bit, puis le neg sur le 8
 
 */
+
+void	jifas(t_list **stack_a, t_list **stack_b)
+{
+	t_place	*plc;
+
+	init_place(stack_a, stack_b, plc);
+}
+
+void init_place(t_list **stack_a, t_list **stack_b, t_place *plc)
+{
+	plc->top_a = *stack_a;
+	plc->bot_a = ft_lstlast(stack_a);
+	plc->top_a = *stack_b;
+	plc->bot_a = ft_lstlast(stack_b);
+}
+
+void compare(t_list **stack_a, t_list **stack_b)
+{
+
+}
+
+void	find_median(t_list **stack_a)
+{
+
+}
